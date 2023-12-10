a1b5f9924a0cb99d49393c9857cba5cd57bd8fbd
inoue_r <chru21072@g.nihon-u.ac.jp>
2023-12-11 03:25:10 +0900

    Tool tipを作成

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,42 +1,62 @@
 import { useState } from "react";
 
 const Icon = () => {
-  const [isHovered, setIsHovered] = useState(false);
-  const image_png =
+  const [showTooltip, setShowTooltip] = useState(false);
+  const imagePng =
     "https://cdn.akamai.steamstatic.com/steam/apps/1097150/header.jpg?t=1698763175";
+
+  const gameTitle = "Fall Guys";
 
   const handleIconClick = () => {
     console.log("クリックされました");
   };
 
   return (
-    <button
-      onClick={handleIconClick}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      style={{
-        padding: 0,
-        backgroundColor: "transparent",
-        border: "none",
-        cursor: "pointer",
-        borderRadius: "50%",
-        overflow: "hidden",
-        width: "100px",
-        height: "100px",
-      }}
-    >
-      <img
-        src={image_png}
-        alt="my image"
-        draggable={false}
+    <div style={{ position: "relative", display: "inline-block" }}>
+      <button
+        onClick={handleIconClick}
+        onMouseEnter={() => setShowTooltip(true)}
+        onMouseLeave={() => setShowTooltip(false)}
         style={{
-          width: "100%",
-          height: "100%",
-          objectFit: "cover",
-          filter: isHovered ? "brightness(110%)" : "brightness(100%)",
+          padding: 0,
+          backgroundColor: "transparent",
+          border: "none",
+          cursor: "pointer",
+          borderRadius: "50%",
+          overflow: "hidden",
+          width: "100px",
+          height: "100px",
         }}
-      />
-    </button>
+      >
+        <img
+          src={imagePng}
+          alt="my image"
+          draggable={false}
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+          }}
+        />
+      </button>
+      {showTooltip && (
+        <div
+          style={{
+            position: "absolute",
+            top: "100%",
+            left: "50%",
+            transform: "translateX(-50%)",
+            backgroundColor: "rgba(0, 0, 0, 0.8)",
+            color: "#fff",
+            padding: "8px",
+            borderRadius: "4px",
+            fontSize: "14px",
+          }}
+        >
+          {gameTitle}
+        </div>
+      )}
+    </div>
   );
 };
 
