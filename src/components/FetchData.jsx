bdd33151cbcb97bcc5798c1fd8ba4922da9bd06c
Your Name <you@example.com>
2024-01-17 13:29:20 +0900

    update fetchData

--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -2,39 +2,34 @@ import jsonData from "./../../data.json";
 
 const FetchData = (props) => {
   const { setData } = props;
-  const data = jsonData;
+  const gameId = 1097150;
+  const GCPurl = "https://us-central1-district-391309.cloudfunctions.net/test";
 
-  setData(data);
+  const fetchData = async () => {
+    try {
+      const response = await fetch(GCPurl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ gameId }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      
+      console.log(data);
+      setData(jsonData);
+    } catch (error) {
+      console.error("Error fetching data:", error.message);
+    }
+  };
+
+  fetchData();
 };
 
-// const FetchData = ({ setData }) => {
-//   const fetchData = async () => {
-//     try {
-//       const response = await fetch(
-//         "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
-//       );
-//       const data = await response.json();
-
-//       // Assuming the data structure includes an 'applist' property with 'apps' array
-//       const gameList = data.applist.apps;
-
-//       // Find the game with the name "Fall Guys"
-//       const fallGuysGame = gameList.find((game) => game.name === "Fall Guys");
-
-//       // Set the data to the found game or handle the case when the game is not found
-//       if (fallGuysGame) {
-//         setData(fallGuysGame);
-//       } else {
-//         console.error("Fall Guys not found in the game list.");
-//       }
-//     } catch (error) {
-//       console.error("Error fetching data:", error);
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-
-//   fetchData();
-// };
 
 export default FetchData;
