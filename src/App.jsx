7476d024860075c0795894bfb5a459383c2747f5
Your Name <you@example.com>
2024-01-25 22:51:42 +0900

    return backlog

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ const App = () => {
     if (data.length !== 0) {
       setTFIDF(TfIdf(data));
     }
-  }, [data,TFIDF]);
+  }, [data]);
 
   useEffect(() => {
     data.forEach((item, index) => {
