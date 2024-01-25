da5cccfb44fc6589cdb3d90fbefc4bf8b9aae61f
Your Name <you@example.com>
2024-01-25 17:44:26 +0900

    test to run fetch func

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ const App = () => {
     if (data.length !== 0) {
       setTFIDF(TfIdf(data));
     }
-  }, [data]);
+  }, [data,TFIDF]);
 
   useEffect(() => {
     data.forEach((item, index) => {
