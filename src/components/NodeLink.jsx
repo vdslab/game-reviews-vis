1bf514d870bf1f5927d603db3266f34a48e9003d
inoue_r <chru21072@g.nihon-u.ac.jp>
2024-01-26 16:42:36 +0900

    ズーム時再レンダリングの防止

--- src/components/NodeLink.jsx
+++ src/components/NodeLink.jsx
@@ -3,17 +3,35 @@ import * as d3 from "d3";
 
 import Icon from "./Icon";
 
+const ZoomableSVG = (props) => {
+  const { children } = props;
+
+  const svgRef = useRef();
+  const [k, setK] = useState(1);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  useEffect(() => {
+    const zoom = d3.zoom().on("zoom", (event) => {
+      const { x, y, k } = event.transform;
+      setX(x);
+      setY(y);
+      setK(k);
+    });
+    d3.select(svgRef.current).call(zoom);
+  }, []);
+  return (
+    <svg ref={svgRef} width={window.innerWidth} height={window.innerHeight}>
+      <g transform={`translate(${x},${y})scale(${k})`}>{children}</g>
+    </svg>
+  );
+};
+
 const NodeLink = (props) => {
   const { data, selectGameIdx, setSelectGameIdx } = props;
-  const svgRef = useRef();
   const k = 5;
 
   const [newLinks, setNewLinks] = useState([]);
   const [newNode, setNewNode] = useState([]);
-
-  const [z, setZ] = useState(1);
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
 
   const nodes = Object.values(data).map((node, index) => ({
     id: index,
@@ -23,6 +41,7 @@ const NodeLink = (props) => {
     TfIdf: node.TfIdf,
     genres: node.genres,
   }));
+
   const calcWeight = (arr1, arr2) => {
     const map1 = new Map();
     const map2 = new Map();
@@ -157,46 +176,36 @@ const NodeLink = (props) => {
     });
   }, []);
 
-  useEffect(() => {
-    const zoom = d3.zoom().on("zoom", (event) => {
-      const { x, y, k } = event.transform;
-      setX(x);
-      setY(y);
-      setZ(k);
-    });
-    d3.select(svgRef.current).call(zoom);
-  }, []);
+  console.log(1);
 
   return (
-    <svg ref={svgRef} width={window.innerWidth} height={window.innerHeight}>
-      <g transform={`translate(${x},${y})scale(${z})`}>
-        {newLinks.length !== 0 &&
-          newLinks.map((link, i) => (
-            <line
-              key={i}
-              className="link"
-              x1={link.source.x}
-              y1={link.source.y}
-              x2={link.target.x}
-              y2={link.target.y}
-              style={{ stroke: link.color, strokeWidth: link.width }}
-            />
-          ))}
-        {newNode.length !== 0 &&
-          newNode.map((node, i) => {
-            return (
-              <g transform={`translate(${node.x},${node.y})`} key={i}>
-                <Icon
-                  name={node.name}
-                  header_image={node.header_image}
-                  index={node.index}
-                  setSelectGameIdx={setSelectGameIdx}
-                ></Icon>
-              </g>
-            );
-          })}
-      </g>
-    </svg>
+    <ZoomableSVG>
+      {newLinks.length !== 0 &&
+        newLinks.map((link, i) => (
+          <line
+            key={i}
+            className="link"
+            x1={link.source.x}
+            y1={link.source.y}
+            x2={link.target.x}
+            y2={link.target.y}
+            style={{ stroke: link.color, strokeWidth: link.width }}
+          />
+        ))}
+      {newNode.length !== 0 &&
+        newNode.map((node, i) => {
+          return (
+            <g transform={`translate(${node.x},${node.y})`} key={i}>
+              <Icon
+                name={node.name}
+                header_image={node.header_image}
+                index={node.index}
+                setSelectGameIdx={setSelectGameIdx}
+              ></Icon>
+            </g>
+          );
+        })}
+    </ZoomableSVG>
   );
 };
 
